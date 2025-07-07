4e0fcc08ad3ae0887a26c3451745348cc8275de0
PredFinance <predfi.xyz@gmail.com>
2025-07-07 06:15:19 -0600

    feat: Update headline font in Tailwind configuration to use Lexend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['var(--font-inter)', 'sans-serif'],
-        headline: ['var(--font-roboto-slab)', 'serif'],
+        headline: ['var(--font-lexend)', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
